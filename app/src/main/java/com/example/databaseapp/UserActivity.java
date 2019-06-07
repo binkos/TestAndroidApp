da9a0efc661828112dcd1761ae47890e8ac7ad7a
binkos <31069701+binkos@users.noreply.github.com>
2019-06-07 03:09:20 +0300

    Can save picture from camera to DB Cant get it at ListView and user's activity

--- app/src/main/java/com/example/databaseapp/UserActivity.java
+++ app/src/main/java/com/example/databaseapp/UserActivity.java
@@ -7,13 +7,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.ImageView;
 import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
+import java.io.IOException;
+
 
 public class UserActivity extends AppCompatActivity {
 
@@ -24,6 +26,11 @@ public class UserActivity extends AppCompatActivity {
     EditText etName;
     EditText etYear;
     Button delButton;
+    ImageView ivIMG;
+    ByteArrayOutputStream stream;
+    byte[] byteArray;
+    Bundle bundle;
+    Bitmap cameraPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class UserActivity extends AppCompatActivity {
         delButton = findViewById(R.id.delete);
         etName = findViewById(R.id.nameField);
         etYear = findViewById(R.id.yearField);
+        ivIMG = findViewById(R.id.ivImg);
 
         dataBaseHelper = new DataBaseHelper(this);
         db = dataBaseHelper.getWritableDatabase();
@@ -46,19 +54,26 @@ public class UserActivity extends AppCompatActivity {
 
         etName.setText(cursor.getString(1));
         etYear.setText(String.valueOf(cursor.getInt(2)));
+     //   ivIMG.setImageBitmap(BitmapFactory.decodeByteArray(cursor.getBlob(3),0,cursor.getBlob(3).length));
 
         cursor.close();
         }
         else {
             delButton.setVisibility(View.GONE);
         }
+
+        ivIMG.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,1);
+        });
+
     }
 
     public void addBut(View view){
         ContentValues contentValues = new ContentValues();
         contentValues.put("name",etName.getText().toString());
         contentValues.put("year",etYear.getText().toString());
-
+        contentValues.put("image",byteArray);
 
         if (userId>0){
             db.update(DataBaseHelper.DATABASE,contentValues,DataBaseHelper.COLUMN_ID+"=?",new String[]{String.valueOf(userId)});
@@ -76,8 +91,30 @@ public class UserActivity extends AppCompatActivity {
 
     public void goHome(){
         db.close();
+        bundle.clear();
+        cameraPicture.recycle();
+        try {
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         startActivity(new Intent(this,MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
     }
 
+    @Override
+    public void onActivityResult(int reqCode,int resultCode, Intent data){
+        if(data==null){return;}
+        if (resultCode==RESULT_OK&&reqCode==1) {
 
+                bundle = data.getExtras();
+                
+                cameraPicture = (Bitmap) bundle.get("data");
+                stream = new ByteArrayOutputStream();
+                cameraPicture.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byteArray = stream.toByteArray();
+                ivIMG.setImageBitmap(cameraPicture);
+
+
+        }
+    }
 }
