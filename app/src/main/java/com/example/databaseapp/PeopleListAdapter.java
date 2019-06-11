1b26d75cbeef3d0415a19db1cb178071fc8f6bba
binkos <31069701+binkos@users.noreply.github.com>
2019-06-12 00:15:57 +0300

    i don't wanna describe it, it has bugs and i'am crying

--- app/src/main/java/com/example/databaseapp/PeopleListAdapter.java
+++ app/src/main/java/com/example/databaseapp/PeopleListAdapter.java
@@ -6,21 +6,22 @@ import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class PeopleListAdapter extends BaseAdapter {
+import androidx.annotation.RecentlyNonNull;
 
-    private Context context;
+public class PeopleListAdapter extends ArrayAdapter<People> {
+
     private int layout;
     private LayoutInflater layoutInflater;
     private ArrayList<People> peopleList;
 
     PeopleListAdapter(Context context, int layout, ArrayList<People> peopleList) {
+        super(context,layout,peopleList);
         this.layout = layout;
         this.peopleList = peopleList;
         this.layoutInflater = LayoutInflater.from(context);
@@ -32,7 +33,7 @@ public class PeopleListAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public People getItem(int position) {
         return peopleList.get(position);
     }
 
@@ -47,16 +48,18 @@ public class PeopleListAdapter extends BaseAdapter {
 
     }
 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder = new ViewHolder();
 
         if (convertView ==null){
-            convertView = layoutInflater.inflate(this.layout,null,false);
+            convertView = layoutInflater.inflate(this.layout,parent,false);
 
             viewHolder.imageView = convertView.findViewById(R.id.ivImg);
             viewHolder.txtName = convertView.findViewById(R.id.nameField);
             viewHolder.txtYear = convertView.findViewById(R.id.yearField);
+
             convertView.setTag(viewHolder);
 
         }
@@ -64,12 +67,13 @@ public class PeopleListAdapter extends BaseAdapter {
 
         People people = peopleList.get(position);
 
-        viewHolder.txtName.setText(people.getName());
-        viewHolder.txtYear.setText(people.getYear());
+        try{viewHolder.txtName.setText(people.getName());}catch (NullPointerException n){n.printStackTrace();}
+        try{viewHolder.txtYear.setText(people.getYear());}catch (NullPointerException n){n.printStackTrace();}
 
-        byte[] bytes = people.getImg();
+        try{byte[] bytes = people.getImg();
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
         viewHolder.imageView.setImageBitmap(bitmap);
+        }catch (NullPointerException n){n.printStackTrace();}
 
         return convertView;
     }
